b5c07025ca8383e56d59fa2ccd85805037ff3d85
wesdevclimb <wesleyjohnson.stu@gmail.com>
2017-02-16 22:11:58 -0500

    Instantiated the Context Class in program.cs

--- ComicBookGalleryModel/Program.cs
+++ ComicBookGalleryModel/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ComicBookGalleryModel.Models;
 
 namespace ComicBookGalleryModel
 {
@@ -10,6 +11,24 @@ namespace ComicBookGalleryModel
     {
         static void Main(string[] args)
         {
+            using (var context = new Context())
+            {
+                context.ComicBooks.Add(new ComicBook()
+                {
+                    SeriesTitle = "The Amazing Spider-Man",
+                    IssueNumber = 1,
+                    PublishedOn = DateTime.Today
+                });
+                context.SaveChanges();
+
+                var comicBooks = context.ComicBooks.ToList();
+                foreach (var comicBook in comicBooks)
+                {
+                    Console.WriteLine(comicBook.SeriesTitle);
+                }
+
+                Console.ReadLine();
+            }
         }
     }
 }
