bccbb948fec98429d509958620f60e2ed8e891dc
wesdevclimb <wesleyjohnson.stu@gmail.com>
2017-02-16 20:55:04 -0500

    Added context class

---
+++ ComicBookGalleryModel/Context.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ComicBookGalleryModel.Models;
+
+namespace ComicBookGalleryModel
+{
+    class Context : DbContext
+    {
+        public DbSet<ComicBook> ComicBooks { get; set; }
+    }
+}
