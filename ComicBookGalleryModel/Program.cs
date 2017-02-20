70fe19df53bf1f01f69b6a34f42819574779a3cd
wesdevclimb <wesleyjohnson.stu@gmail.com>
2017-02-20 14:29:12 -0500

    Testing linq queries

--- ComicBookGalleryModel/Program.cs
+++ ComicBookGalleryModel/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Data.Entity;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -14,91 +15,27 @@ namespace ComicBookGalleryModel
         {
             using (var context = new Context())
             {
-                var series1 = new Series()
-                {
-                    Title = "The Amazing Spider-Man"
-                };
-                var series2 = new Series()
-                {
-                    Title = "The Invincible Iron Man"
-                };
+                context.Database.Log = (message) => Debug.WriteLine(message);
 
-                var stanLee = new Artist()
-                {
-                    Name = "Stan Lee"
-                };
+                var comicBooks = context.ComicBooks.ToList();
 
-                var jackKirby = new Artist()
-                {
-                    Name = "Jack Kirby"
-                };
+                Console.WriteLine("# of comicbooks: {0}", comicBooks.Count);
 
-                var steveDitko = new Artist()
-                {
-                    Name = "Steve Ditko"
-                };
-
-                var role1 = new Role()
-                {
-                    Name = "Script"
-                };
-
-                var role2 = new Role()
-                {
-                    Name = "Pencils"
-                };
-
-                var comicBook1 = new ComicBook()
-                {
-                    Series = series1,
-                    IssueNumber = 1,
-                    PublishedOn = DateTime.Today
-                };
-
-                comicBook1.AddArtist(stanLee, role1);
-                comicBook1.AddArtist(jackKirby, role2);
-
-                var comicBook2 = new ComicBook()
-                {
-                    Series = series1,
-                    IssueNumber = 2,
-                    PublishedOn = DateTime.Today
-                };
-
-                comicBook2.AddArtist(stanLee, role1);
-                comicBook2.AddArtist(jackKirby, role2);
+                //var comicBooks = context.ComicBooks
+                //    .Include(cb => cb.Series)
+                //    .Include(cb => cb.Artists.Select(a => a.Artist))
+                //    .Include(cb => cb.Artists.Select(a => a.Role))
+                //    .ToList();
+                //foreach (var comicBook in comicBooks)
+                //{
+                //    var artistRoleNames = comicBook.Artists
+                //        .Select(a => $"{a.Artist.Name} - {a.Role.Name}").ToList();
+                //    var artistRolesDisplayText = string.Join(", ", artistRoleNames);
 
 
-                var comicBook3 = new ComicBook()
-                {
-                    Series = series2,
-                    IssueNumber = 1,
-                    PublishedOn = DateTime.Today
-                };
-
-                comicBook3.AddArtist(stanLee, role1);
-                comicBook3.AddArtist(steveDitko, role2);
-
-                context.ComicBooks.Add(comicBook1);
-                context.ComicBooks.Add(comicBook2);
-                context.ComicBooks.Add(comicBook3);
-                context.SaveChanges();
-
-                var comicBooks = context.ComicBooks
-                    .Include(cb => cb.Series)
-                    .Include(cb => cb.Artists.Select(a => a.Artist))
-                    .Include(cb => cb.Artists.Select(a => a.Role))
-                    .ToList();
-                foreach (var comicBook in comicBooks)
-                {
-                    var artistRoleNames = comicBook.Artists
-                        .Select(a => $"{a.Artist.Name} - {a.Role.Name}").ToList();
-                    var artistRolesDisplayText = string.Join(", ", artistRoleNames);
-
-
-                    Console.WriteLine(comicBook.DisplayText);
-                    Console.WriteLine(artistRolesDisplayText);
-                }
+                //    Console.WriteLine(comicBook.DisplayText);
+                //    Console.WriteLine(artistRolesDisplayText);
+                //}
 
                 Console.ReadLine();
             }
