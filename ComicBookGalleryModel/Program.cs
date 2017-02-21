7e3302fd08b7728f5a033b5ce13689c07bc41d48
wesdevclimb <wesleyjohnson.stu@gmail.com>
2017-02-21 17:22:15 -0500

    Lots of stuff changed

--- ComicBookGalleryModel/Program.cs
+++ ComicBookGalleryModel/Program.cs
@@ -17,14 +17,17 @@ namespace ComicBookGalleryModel
             {
                 context.Database.Log = (message) => Debug.WriteLine(message);
 
-                var comicBooks = context.ComicBooks.ToList();
 
-                Console.WriteLine("# of comicbooks: {0}", comicBooks.Count);
+                var comicBookId = 1;
+                var comicBook1 = context.ComicBooks.Find(comicBookId);
+                var comicBook2 = context.ComicBooks.Find(comicBookId);
+
+
 
                 //var comicBooks = context.ComicBooks
-                //    .Include(cb => cb.Series)
-                //    .Include(cb => cb.Artists.Select(a => a.Artist))
-                //    .Include(cb => cb.Artists.Select(a => a.Role))
+                //    //.Include(cb => cb.Series)
+                //    //.Include(cb => cb.Artists.Select(a => a.Artist))
+                //    //.Include(cb => cb.Artists.Select(a => a.Role))
                 //    .ToList();
                 //foreach (var comicBook in comicBooks)
                 //{
